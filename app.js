108383f0386e808e97c360ccbc829de69b94a1b3
XCA Student <xcastudent@Admins-MacBook-Pro.local>
2022-06-23 15:57:56 -0400

    xca/indexandindex2working

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const bodyParser = require('body-parser')
 const { MongoClient, ServerApiVersion } = require('mongodb')
 const fetch = require('node-fetch')
 const { clusterPassword } = require('./config')
-
-
 const app = express()
 
 app.set('view engine', 'pug')
 app.set('views', "./views")
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use('/styles', express.static('styles'))
 app.use('/images', express.static('public'))
 app.use(express.static('public'))
-app.use(bodyParser.json())
-
 
 const uri = `mongodb+srv://${clusterPassword}@cluster0.fxgx6.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -42,14 +39,16 @@ client.connect(err => {
             console.log(result)
             res.redirect("/")
         }).catch(error => console.log(error))
-
     })
-
-    // perform actions on the collection object
+    app.put("/upvote/:id", (req, res) => {
+            console.log(req.params)
+        })
+        // perform actions on the collection object
     app.put('/murals', (req, res) => {
         console.log(req.body)
-        muralCollection.findOneAndUpdate({ muralTitle: `test ${req.body.muralTitle}` }, {
+        muralCollection.findOneAndUpdate({ muralTitle: `Sherwin ${req.body.muralTitle}` }, {
             $set: {
+                id: req.body.id,
                 muralTitle: req.body.muralTitle,
                 image: req.body.image
             }
@@ -64,10 +63,11 @@ client.connect(err => {
         console.log(req.body)
         muralCollection.insertOne(req.body).then(result => {
             console.log(result)
-            res.redirect("/")
+            muralCollection.find().toArray().then(results => {
+                console.log(results)
+                res.render('index2', { images: results })
+            })
         }).catch(error => console.log(error))
-        res.render('index2')
     })
-
     app.listen(3000, () => console.log('Server is Serving'))
 });
--- public/script.js
+++ public/script.js
@@ -1,40 +1,5 @@
-function addToCount() {
-    let countAsString = document.querySelector('#count').innerHTML
-    let countAsInteger = parseInt(countAsString)
-    let newCount = countAsInteger += 1
-    document.querySelector('#count').innerHTML = newCount
-
+function addToCount(button) {
+    let imageId = button.value
+    fetch(`/upvote/${imageId}`, { method: 'PUT' })
+    console.log(imageId)
 }
-
-function subtractFromCount() {
-    let countAsString = document.querySelector('#count').innerHTML
-    let countAsInteger = parseInt(countAsString)
-    let newCount = countAsInteger -= 1
-    document.querySelector('#count').innerHTML = newCount
-}
-
-const likeButton = document.querySelector('#like-button')
-const dislikeButton = document.querySelector('#dislike-button')
-
-likeButton.addEventListener('click', _ => {
-
-    fetch('/murals', {
-        method: 'put',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify()
-    }).then(response => {
-        console.log(response)
-        window.location.reload(true)
-    })
-})
-dislikeButton.addEventListener('click', _ => {
-
-    fetch('/murals', {
-        method: 'put',
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify()
-    }).then(response => {
-        console.log(response)
-        window.location.reload(true)
-    })
-})
